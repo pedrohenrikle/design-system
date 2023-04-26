2baddec6375d60cb06e47f53951115e1f23f89be
pedrohenrikle <pedro.klein.sl@gmail.com>
2023-04-26 16:51:21 -0300

    fix: Downgrade some packages

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   fontSizes,
   fontWeights,
   lineHeights,
-} from '@ignite-ui/tokens'
+} from '@pedrohenrikle-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
